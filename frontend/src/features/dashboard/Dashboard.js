a4a4b3d807496cc5f40fbba4bdf1d1c81279c973
Raghav Muchhal <raghavmuchhal005@gmail.com>
2025-05-17 04:42:38 +0530

    Filter for Admin Prod

--- frontend/src/features/dashboard/Dashboard.js
+++ frontend/src/features/dashboard/Dashboard.js
@@ -23,7 +23,7 @@ const Dashboard = () => {
     let filtered = products;
 
     if (role === 'admin') {
-      filtered = products.filter((product) => product.admin === username);
+      filtered = products.filter((product) => product.createdBy === username);
     }
 
     setFilteredProducts(filtered);
@@ -37,7 +37,7 @@ const Dashboard = () => {
     let filtered = products;
 
     if (role === 'admin') {
-      filtered = products.filter((product) => product.admin === username);
+      filtered = products.filter((product) => product.createdBy === username);
     }
 
     if (searchTerm) {
